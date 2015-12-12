314d91134b23d85299933e183492ae40fc2a95ec
choihyeryeong <hr_95@naver.com>
2015-12-12 16:03:24 +0900

    1주차 실습 로그인

---
+++ app/src/main/java/kookmin/ac/kr/m1_login/MainActivity.java
@@ -0,0 +1,84 @@
+package kookmin.ac.kr.m1_login;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import kookmin.ac.kr.m1_login.LoginActivity;
+
+public class MainActivity extends Activity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Log.d("TEST", "oncreate1");
+        setContentView(R.layout.activity_main);
+        Button bt = (Button)this.findViewById(R.id.btnNew);
+        Button LogBt = (Button)this.findViewById(R.id.strTestBtn);
+
+
+/*
+        //익명 inner class 임시객체
+        //1
+        bt.setOnClickListener(new View.OnClickListener(){
+                                  @Override
+
+                                  public void onClick(View v) {
+                                      Toast.makeText(MainActivity.this.getApplicationContext(), "Click Button", Toast.LENGTH_LONG).show();
+                                  }
+
+                              }
+        );
+
+
+        */
+
+        // 익명 클래스
+        //2
+        // bt.setOnClickListener(btnlistener2);
+
+
+        /*
+            //3 리스너 구현
+            ButtonClickListener btnlistener = new ButtonClickListener();
+                bt.setOnClickListener(btnlistener);
+         */
+
+    }
+    //2 익명 클래스
+    View.OnClickListener btnlistener2 = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Toast.makeText(MainActivity.this.getApplicationContext(), "Click Button", Toast.LENGTH_LONG).show();
+        }
+    };
+
+    /*
+    //3 리스너 구현
+    class ButtonClickListener implements View.OnClickListener {
+        @Override
+        public void onClick(View v) {
+            Toast.makeText(MainActivity.this.getApplicationContext(), "Click Button", Toast.LENGTH_LONG).show();
+        }
+    }
+    */
+
+    public void onTestBtnClicked(View v) {
+        Intent intent = new Intent(this,LoginActivity.class);
+        startActivity(intent);
+    }
+
+    public void onNewBtnClicked(View v) {
+        Toast.makeText(getApplicationContext(), "click button2.", Toast.LENGTH_LONG).show();
+    }
+}
+
+
